f9084396a373480dbba4a465d877520f6a82102b
Michael Hyer <michaelhy2002@gmail.com>
2024-03-07 23:00:58 -0700

    Completed W09 Learning Activity

---
+++ links.js
@@ -0,0 +1,33 @@
+let baseURL = "https://seriousmikey.github.io/wdd230/";
+let linksURL = "https://seriousmikey.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+    i = 1;
+    weeks.forEach(week => {
+        let lessonNumber = document.getElementById(`0${i}`);
+
+        let linksVar = week.links;
+    
+        let number = 0;
+        linksVar.forEach(link => {
+            let aElement = document.createElement("a");
+            aElement.setAttribute("href", linksVar[number].url);
+            aElement.setAttribute("target", "_blank");
+            aElement.textContent = linksVar[number].title;
+    
+            lessonNumber.appendChild(aElement);
+    
+            number++;
+        });
+        i++;
+    });
+};
+
+getLinks();
